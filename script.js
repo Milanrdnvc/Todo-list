16dfd35c2403a26706a4fdc15ab15ad2eab16476
Milanrdnvc <milanradinovic@rocketmail.com>
2020-08-10 18:07:54 +0200

    Changed JS code to use JSON object

--- script.js
+++ script.js
@@ -1,20 +1,8 @@
-let itemCount = 0;
-let itemDelete;
-loadItemsFromLocalStorage();
-
 const input = document.querySelector('input');
 const ul = document.querySelector('ul');
-const todoAdd = document.querySelector('.todo-add');
-
-
-for (let item in localStorage) {
-    if (/item/.test(item)) {
-        itemCount++;
-    }
-}
 
 input.addEventListener('keydown', addItem);
-todoAdd.addEventListener('click', addItem);
+loadItemsFromLocalStorage();
 
 function addItem(e) {
     if ((e.key === 'Enter' || e.target.classList[1] === 'todo-add') && input.value !== '') {
@@ -26,25 +14,21 @@ function addItem(e) {
                         </div>`;
         ul.appendChild(li);
         input.value = '';
-        localStorage.setItem(`item${itemCount}`, li.innerHTML);
-        itemCount++;
+        if (!localStorage.getItem('todoItem')) {
+            localStorage.setItem('todoItem', JSON.stringify([li.innerHTML]));
+        } else {
+            let oldItems = JSON.parse(localStorage.getItem('todoItem'));
+            oldItems.push(li.innerHTML);
+            localStorage.setItem('todoItem', JSON.stringify(oldItems));
+        }
     } else return;
 }
 
 function loadItemsFromLocalStorage() {
-    for (let i = 0; i < itemCount; i++) {
+    for (let i = 0; i < JSON.parse(localStorage.getItem('todoItem')).length; i++) {
         let li = document.createElement('li');
-        li.innerHTML = localStorage.getItem(`item${i}`);
+        li.innerHTML = JSON.parse(localStorage.getItem('todoItem'))[i];
         ul.appendChild(li);
-    }
-
-    itemDelete = document.querySelectorAll('.item-delete');
-    itemDelete.forEach(item => {
-        item.addEventListener('click', () => {
-            item.parentElement.parentElement.remove();
-
-        });
-    });   
+    } 
 }
-
 
