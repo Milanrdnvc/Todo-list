9509a5c82ee693f9b95aec7feada2bcb50e3fabf
Milanrdnvc <milanradinovic@rocketmail.com>
2020-08-10 18:53:23 +0200

    Implemented remove task button

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const input = document.querySelector('input');
 const ul = document.querySelector('ul');
+const addTodoBtn = document.querySelector('.todo-add');
+let itemDelete;
 
-input.addEventListener('keydown', addItem);
 loadItemsFromLocalStorage();
+addDeleteEvent();
+input.addEventListener('keydown', addItem);
+addTodoBtn.addEventListener('click', addItem);
 
 function addItem(e) {
     if ((e.key === 'Enter' || e.target.classList[1] === 'todo-add') && input.value !== '') {
@@ -16,10 +20,13 @@ function addItem(e) {
         input.value = '';
         if (!localStorage.getItem('todoItem')) {
             localStorage.setItem('todoItem', JSON.stringify([li.innerHTML]));
+            addDeleteEvent();
         } else {
             let oldItems = JSON.parse(localStorage.getItem('todoItem'));
             oldItems.push(li.innerHTML);
             localStorage.setItem('todoItem', JSON.stringify(oldItems));
+            itemDelete = document.querySelectorAll('.item-delete');
+            addDeleteEvent();
         }
     } else return;
 }
@@ -29,6 +36,19 @@ function loadItemsFromLocalStorage() {
         let li = document.createElement('li');
         li.innerHTML = JSON.parse(localStorage.getItem('todoItem'))[i];
         ul.appendChild(li);
-    } 
+    }
+
+    itemDelete = document.querySelectorAll('.item-delete');
+}
+
+function addDeleteEvent() {
+    itemDelete.forEach((item, index) => {
+        item.onclick = function() {
+            item.parentNode.parentNode.remove();
+            let todoItems = JSON.parse(localStorage.getItem('todoItem'));
+            todoItems.splice(index, 1);
+            localStorage.setItem('todoItem', JSON.stringify(todoItems));
+        }
+    });
 }
 
