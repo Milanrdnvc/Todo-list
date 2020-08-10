cfb8b35269a33e8df1c010efd05eb2e901fcb85a
Milanrdnvc <milanradinovic@rocketmail.com>
2020-08-10 20:51:01 +0200

    Modified JS, separated into fn.s

--- script.js
+++ script.js
@@ -1,54 +1,79 @@
 const input = document.querySelector('input');
 const ul = document.querySelector('ul');
 const addTodoBtn = document.querySelector('.todo-add');
-let itemDelete;
+let itemDeleteButton;
 
 loadItemsFromLocalStorage();
-addDeleteEvent();
-input.addEventListener('keydown', addItem);
-addTodoBtn.addEventListener('click', addItem);
+input.addEventListener('keydown', addItemFromInput);
+addTodoBtn.addEventListener('click', addItemFromInput);
 
-function addItem(e) {
+function appendToDOMAndLS(inputValue) {
+    let li = document.createElement('li');
+    li.innerHTML = `${inputValue}
+                    <div class="delete-complete-wrapper">
+                        <span class="item-complete">&#10003;</span>
+                        <span class="item-delete">&times;</span>
+                    </div>`;
+    ul.appendChild(li);
+    if (!localStorage.getItem('todoList')) {
+        localStorage.setItem('todoList', JSON.stringify([li.innerHTML]));
+    } else {
+        let oldList = JSON.parse(localStorage.getItem('todoList'));
+        oldList.push(li.innerHTML);
+        localStorage.setItem('todoList', JSON.stringify(oldList));
+    }
+
+    itemDeleteButton = document.querySelectorAll('.item-delete');
+    deleteItem(itemDeleteButton);
+}
+
+function addItemFromInput(e) {
     if ((e.key === 'Enter' || e.target.classList[1] === 'todo-add') && input.value !== '') {
-        let li = document.createElement('li');
-        li.innerHTML = `${input.value}
-                        <div class="delete-complete-wrapper">
-                            <span class="item-complete">&#10003;</span>
-                            <span class="item-delete">&times;</span>
-                        </div>`;
-        ul.appendChild(li);
+        appendToDOMAndLS(input.value);
         input.value = '';
-        if (!localStorage.getItem('todoItem')) {
-            localStorage.setItem('todoItem', JSON.stringify([li.innerHTML]));
-            addDeleteEvent();
-        } else {
-            let oldItems = JSON.parse(localStorage.getItem('todoItem'));
-            oldItems.push(li.innerHTML);
-            localStorage.setItem('todoItem', JSON.stringify(oldItems));
-            itemDelete = document.querySelectorAll('.item-delete');
-            addDeleteEvent();
-        }
     } else return;
 }
 
 function loadItemsFromLocalStorage() {
-    for (let i = 0; i < JSON.parse(localStorage.getItem('todoItem')).length; i++) {
-        let li = document.createElement('li');
-        li.innerHTML = JSON.parse(localStorage.getItem('todoItem'))[i];
-        ul.appendChild(li);
-    }
+    if (localStorage.getItem('todoList')) {
+        // ul.querySelectorAll('li').forEach(item => item.remove());
+        for (let i = 0; i < JSON.parse(localStorage.getItem('todoList')).length; i++) {
+            let li = document.createElement('li');
+            li.innerHTML = JSON.parse(localStorage.getItem('todoList'))[i];
+            ul.appendChild(li);
+        }
 
-    itemDelete = document.querySelectorAll('.item-delete');
+        itemDeleteButton = document.querySelectorAll('.item-delete');
+        deleteItem(itemDeleteButton);
+    }
 }
 
-function addDeleteEvent() {
-    itemDelete.forEach((item, index) => {
-        item.onclick = function() {
-            item.parentNode.parentNode.remove();
-            let todoItems = JSON.parse(localStorage.getItem('todoItem'));
-            todoItems.splice(index, 1);
-            localStorage.setItem('todoItem', JSON.stringify(todoItems));
+function deleteItem(delBtn) {
+    delBtn.forEach(btn => {
+        if (!btn.onclick) {
+            btn.onclick = function() {
+                btn.parentNode.parentNode.remove();
+                // REMOVE FROM LOCAL STORAGE
+            }
         }
     });
 }
 
+// function updateItemDelete() {
+//     itemDelete = document.querySelectorAll('.item-delete');
+//     itemDelete.forEach((item, index) => {
+//         // item.addEventListener('click', () => {
+//         //     item.parentNode.parentNode.remove();
+//         //     let todoLists = JSON.parse(localStorage.getItem('todoList'));
+//         //     // todoLists.splice(index, 1);
+//         //     console.log(todoLists);
+//         // });
+//         if (!item.onclick) {
+//             item.onclick = function() {
+//                 item.parentNode.parentNode.remove();
+//                 let todoLists = JSON.parse(localStorage.getItem('todoList'));
+//                 localStorage.removeItem('todoList')
+//             };
+//         }
+//     });
+// }
